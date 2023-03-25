bb4ccbbc780d3be5c0471069889287827d3e5730
lmchun <lmchun@sbcglobal.net>
2023-03-25 17:35:55 -0400

    fresh code

--- index.js
+++ index.js
@@ -74,19 +74,12 @@
   var scenes = data.scenes.map(function(data) {
     var urlPrefix = "tiles";
     var source = Marzipano.ImageUrlSource.fromString(
-      // urlPrefix + "/" + data.id + "/{z}/{f}/{y}/{x}.jpg",
-       urlPrefix + "/" + data.id + ".jpg",
-      // { cubeMapPreviewUrl: urlPrefix + "/" + data.id + "/preview.jpg" }
-      );
-    /* var geometry = new Marzipano.EquirectGeometry(data.levels);
-     var geometry = new Marzipano.CubeGeometry(data.levels);*/
-    var geometry = new Marzipano.EquirectGeometry([{ width: 4000 }]);
+      urlPrefix + "/" + data.id + "/{z}/{f}/{y}/{x}.jpg",
+      { cubeMapPreviewUrl: urlPrefix + "/" + data.id + "/preview.jpg" });
+    var geometry = new Marzipano.CubeGeometry(data.levels);
 
-    /* var limiter = Marzipano.RectilinearView.limit.traditional(data.faceSize, 100*Math.PI/180, 120*Math.PI/180);
-     var view = new Marzipano.RectilinearView(data.initialViewParameters, limiter);*/
-    // Create view.
-    var limiter = Marzipano.RectilinearView.limit.traditional(1024, 100*Math.PI/180);
-    var view = new Marzipano.RectilinearView({ yaw: Math.PI }, limiter);
+    var limiter = Marzipano.RectilinearView.limit.traditional(data.faceSize, 100*Math.PI/180, 120*Math.PI/180);
+    var view = new Marzipano.RectilinearView(data.initialViewParameters, limiter);
 
     var scene = viewer.createScene({
       source: source,
@@ -99,7 +92,6 @@
     data.linkHotspots.forEach(function(hotspot) {
       var element = createLinkHotspotElement(hotspot);
       scene.hotspotContainer().createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
-
     });
 
     // Create info hotspots.
@@ -107,25 +99,7 @@
       var element = createInfoHotspotElement(hotspot);
       scene.hotspotContainer().createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
     });
-    
-    //create iframe hotspots, grab each element for each element get this property
-    for(i=0; i<data.length; i++){
-    // data.forEach(function(hotspot){
-      console.log(data.scenes[i]);
-      //for each item in array if there is an iFrame Spot, then call the function
-      if(data.iframespot != null ){
-      var element = createiFrameSpotElement(hotspot);
-      scene.hotspotContainer().createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
-      }
-      // });
-    }
-    
-    //   //create iframeselect hotspots.
-    //   data.iframeselect.forEach(function(hotspot){
-    //     var element = createiFrameSelectElement(hotspot);
-    //     scene.hotspotContainer().createHotspot(element, { yaw: hotspot.yaw, pitch: hotspot.pitch });
-  
-    //   });
+
     return {
       data: data,
       scene: scene,
@@ -271,6 +245,7 @@
   }
 
   function createLinkHotspotElement(hotspot) {
+
     // Create wrapper element to hold icon and tooltip.
     var wrapper = document.createElement('div');
     wrapper.classList.add('hotspot');
@@ -382,110 +357,6 @@
     return wrapper;
   }
 
-  function createiFrameSpotElement(hotspot){
-
-    // // Create wrapper element to hold icon and tooltip.
-    // var wrapper = document.createElement('div');
-    // wrapper.classList.add('hotspot');
-    // wrapper.classList.add('video-hotspot');
-
-    // // Create hotspot/tooltip header.
-    // var header = document.createElement('div');
-    // header.classList.add('video-hotspot-header');
-    // wrapper.appendChild(header)
-   /*
-    // Create image element.
-    var iconWrapper = document.createElement('div');
-    iconWrapper.classList.add('info-hotspot-icon-wrapper');
-    var icon = document.createElement('img');
-    icon.src = 'img/info.png';
-    icon.classList.add('info-hotspot-icon');
-    iconWrapper.appendChild(icon);
-
-    // Create title element.
-    var titleWrapper = document.createElement('div');
-    titleWrapper.classList.add('info-hotspot-title-wrapper');
-    var title = document.createElement('div');
-    title.classList.add('info-hotspot-title');
-    title.innerHTML = hotspot.title;
-    titleWrapper.appendChild(title);
-
-    // Create close element.
-    var closeWrapper = document.createElement('div');
-    closeWrapper.classList.add('info-hotspot-close-wrapper');
-    var closeIcon = document.createElement('img');
-    closeIcon.src = 'img/close.png';
-    closeIcon.classList.add('info-hotspot-close-icon');
-    closeWrapper.appendChild(closeIcon);
-
-    // Construct header element.
-    header.appendChild(iconWrapper);
-    header.appendChild(titleWrapper);
-    header.appendChild(closeWrapper);
- */
-    // create Video Interview Div
-    let videoInterview = document.createElement('div')
-    videoInterview.id = 'videoInterview'
-    // header.appendChild(header)
-
-    // create iframespot div
-    let videoLocationDiv = document.createElement('div')
-    videoLocationDiv.id = 'iframespot'
-    videoInterview.appendChild(videoLocationDiv)
-
-    let videoLocationDivMessage = document.createElement('div')
-    videoLocationDivMessage.classList.add('message')
-    videoLocationDiv.appendChild(videoLocationDivMessage)
-    videoLocationDivMessage.innerText = hotspot.message
-
-    /* TODO add the text to div message */
-
-    // Create text element.
-    // var text = document.createElement('div');
-    // text.classList.add('info-hotspot-text');
-    // text.innerHTML = hotspot.text;
-
-    // Place header and text into wrapper element.
-    // wrapper.appendChild(header);
-    // wrapper.appendChild(text);
-
-    // Create a modal for the hotspot content to appear on mobile mode.
-    // var modal = document.createElement('div');
-    // modal.innerHTML = wrapper.innerHTML;
-    // modal.classList.add('info-hotspot-modal');
-    // document.body.appendChild(modal);
-
-    // var toggle = function() {
-    //   wrapper.classList.toggle('visible');
-    //   modal.classList.toggle('visible');
-    // };
-
-    // Show content when hotspot is clicked.
-    // wrapper.querySelector('.info-hotspot-header').addEventListener('click', toggle);
-
-    // Hide content when close icon is clicked.
-    // modal.querySelector('.info-hotspot-close-wrapper').addEventListener('click', toggle);
-
-    // Prevent touch and scroll events from reaching the parent element.
-    // This prevents the view control logic from interfering with the hotspot.
-    // stopTouchAndScrollEventPropagation(wrapper);
-
-    return videoInterview;
-  }
-
-  function createiFrameSelectElement (hotspot){
-     // create iframeselect
-     let videoUISelection = document.createElement('ul')
-     videoUISelection.id = ('iframeselect')
-     //for loop to create the li based on the length of the array
-     for(i=0; i<hotspot.speakers.length; i++){
-      let speakerSelection = document.createElement('li')
-      speakerSelection.setAttribute("data-source", hotspot.speakerID)
-      speakerSelection.innerText = hotspot.speakers
-    }
-    return videoUISelection;
- 
-  }
   // Prevent touch and scroll events from reaching the parent element.
   function stopTouchAndScrollEventPropagation(element, eventList) {
     var eventList = [ 'touchstart', 'touchmove', 'touchend', 'touchcancel',
@@ -519,72 +390,3 @@
   switchScene(scenes[0]);
 
 })();
-
-// Get the hotspot container for scene.
-var container = scene.hotspotContainer();
-
-// Create hotspot with different sources.
-container.createHotspot(document.getElementById('iframespot'), { yaw: 0.0335, pitch: -0.102 },
-  { perspective: { radius: 1640, extraTransforms: "rotateX(5deg)" }});
-container.createHotspot(document.getElementById('iframeselect'), { yaw: -0.35, pitch: -0.239 });
-
-// HTML sources.
-var hotspotHtml = {
-  youtube: '<iframe id="youtube" width="1280" height="480" src="https://www.youtube.com/embed/a4YjKmsXyds?rel=0&amp;controls=0&amp;showinfo=0&amp;" frameborder="0" allowfullscreen></iframe>',
-  youtubeWithControls: '<iframe id="youtubeWithControls" width="1280" height="480" src="https://www.youtube.com/embed/a4YjKmsXyds?rel=0&amp;controls=1&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>',
-  googleMaps: '<iframe id="googlemaps" width="1280" height="480" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9487.563699358636!2d-9.211273541013671!3d38.69789785451112!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1ecf578f4d20e9%3A0x530952e38d140ae6!2sDigisfera+-+Fotografia+e+Inform%C3%A1tica%2C+Lda!5e1!3m2!1spt-PT!2spt!4v1429956174252" width="600" height="450" frameborder="0" style="border:0"></iframe>',
-  koloreyes: '<iframe id="koloreyes" src="https://eyes.kolor.com/video/i/kolor/54ef73cbaaa38b2943c8a7d72a4b00e6" type="text/html" width="1280" height="480" frameborder="0" scrolling="no" allowfullscreen="true"> </iframe>'
-};
-
-// Switch sources when clicked.
-function switchHotspot(id) {
-  var wrapper = document.getElementById('iframespot');
-  wrapper.innerHTML = hotspotHtml[id];
-}
-
-
-// ///////////////
-// // Create hotspot with different sources.
-// container.createHotspot(document.getElementById('iframespot'), { yaw: 0.0335, pitch: -0.102 },
-//   { perspective: { radius: 1640, extraTransforms: "rotateX(5deg)" }});
-// container.createHotspot(document.getElementById('iframeselect'), { yaw: -0.35, pitch: -0.239 });
-
-// // HTML sources.
-// var hotspotHtml = {
-//   TSamuelu: '<iframe width="1280" height="720" src="https://www.youtube.com/embed/gaA7RAy5rYg" title="YouTube video player" frameborder="0"s allowfullscreen></iframe>',
-//   KLatif: '<iframe width="1280" height="720" src="https://www.youtube.com/embed/gaA7RAy5rYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-//   CDang: '<iframe width="1280" height="720" src="https://www.youtube.com/embed/gaA7RAy5rYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-//   CSvetvilas: '<iframe width="1280" height="720" src="https://www.youtube.com/embed/gaA7RAy5rYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-//   AWong: '<iframe width="1280" height="720" src="https://www.youtube.com/embed/gaA7RAy5rYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-//   MAhmed: '<iframe width="1280" height="720" src="https://www.youtube.com/embed/gaA7RAy5rYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-//   CSu: '<iframe width="1280" height="720" src="https://www.youtube.com/embed/gaA7RAy5rYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-//   JVaj: '<iframe width="1280" height="720" src="https://www.youtube.com/embed/gaA7RAy5rYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-//   LFu: '<iframe width="1280" height="720" src="https://www.youtube.com/embed/gaA7RAy5rYg" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>',
-//   googleMaps: '<iframe id="googlemaps" width="1280" height="480" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9487.563699358636!2d-9.211273541013671!3d38.69789785451112!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd1ecf578f4d20e9%3A0x530952e38d140ae6!2sDigisfera+-+Fotografia+e+Inform%C3%A1tica%2C+Lda!5e1!3m2!1spt-PT!2spt!4v1429956174252" width="600" height="450" frameborder="0" style="border:0"></iframe>',
-  
-// };
-
-//UI included OLD <iframe id="youtubeWithControls" width="1280" height="720" src="https://www.youtube.com/embed/gaA7RAy5rYg" frameborder="0" allowfullscreen></iframe>
-
-// Switch sources when clicked.
-function switchHotspot(id) {
-  var wrapper = document.getElementById('iframespot');
-  wrapper.innerHTML = hotspotHtml[id];
-}
-
-var switchElements = document.querySelectorAll('[data-source]');
-for (var i = 0; i < switchElements.length; i++) {
-  var element = switchElements[i];
-  addClickEvent(element);
-}
-
-function addClickEvent(element) {
-  element.addEventListener('click', function() {
-    switchHotspot(element.getAttribute('data-source'));
-  });
-}
-//////////////////////////////////////////////////////////////////
-
-
-
-container.createHotspot(document.querySelector("#tooltip"), { yaw: 4.55, pitch: 0.102});
